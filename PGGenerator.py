bc10edfbb7f384a753a8f4d47aab1ad85dad89ca
Bob <bob.sheets76@gmail.com>
2025-08-20 09:15:15 -0600

    Removed choices that don't allign with current password requirements.

--- PGGenerator.py
+++ PGGenerator.py
@@ -6,28 +6,17 @@ import string
 length = int(input("Enter the length of your password: "))
 
 print('''Choose what kinds of characters you'd like to include in your password:
-1. Uppercase letters
-2. Lowercase letters
-3. Numbers
-4. Symbols
-5. All of the above
+1. Generate a password with uppercase letters, lowercase letters, numbers, and special characters.
 ''')
 
 characterlist = ""
 
 # Getting the character list for the password
 while(True):
-    choice = int(input("Enter your choice (1-5) (We strongly recommend 5): "))
+    choice = int(input("Press 1 to generate your password: "))
     if (choice == 1):
-        characterlist += string.ascii_uppercase
-    elif (choice == 2):
-        characterlist += string.ascii_lowercase
-    elif (choice == 3):
-        characterlist += string.digits
-    elif (choice == 4):
-        characterlist += string.punctuation
-    elif (choice == 5):
         characterlist += string.ascii_letters + string.digits + string.punctuation
+        
     else:
         print("Invalid choice. Please try again.")
     
