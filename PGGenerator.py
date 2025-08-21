1969527d98bf839e47459af024c806d31a3862d7
Bob <bob.sheets76@gmail.com>
2025-08-21 11:35:52 -0600

    removed while true, and if/else code

--- PGGenerator.py
+++ PGGenerator.py
@@ -6,16 +6,13 @@ print('''Generate a password with uppercase letters, lowercase letters, numbers,
       and special characters.''')
 characterlist = ""
 # Getting the character list for the password
-while(True):
-    choice = int(input("Press 1 to generate your password: "))
-    if (choice == 1):
-        characterlist += string.ascii_letters + string.digits + string.punctuation        
-    else:
-        print("Invalid choice. Please try again.")    
-    password = []
-    for i in range(length):
+
+choice = int(input("Press 1 to generate your password: "))
+(choice == 1)
+characterlist += string.ascii_letters + string.digits + string.punctuation        
+password = []
+for i in range(length):
         # Picking a random character from the list
         random_character = random.choice(characterlist)
         password.append(random_character)
-    print("Your randomly generated password is: " + "".join(password))
-    break
+print("Your randomly generated password is: " + "".join(password))
